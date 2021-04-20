32d90b770e131cc3af9910e494b5d0c5ff399ae4
fortizva <fortizva@alumnos.unex.es>
2021-04-20 16:30:32 +0200

    Bugfix DNILetterClient + Fixed compiling errors + Fixed connection errors + Fixed user input system - Removed window - Removed duplicated/unnecesary variables

--- src/pcd/dni/DNILetterClient.java
+++ src/pcd/dni/DNILetterClient.java
@@ -1,76 +1,68 @@
 package pcd.dni;
 
-import java.io.*;
-import java.net.*;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
-import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.SocketException;
-import pcd.util.Ventana;
 import java.util.Scanner;
 
 public class DNILetterClient {
-	
-	private int id;
-	private Socket socket;
 
-	public DNILetterServer(Socket socket, int id) {
-		this.socket = socket;
-		this.id = id;
-	}
+	private static final String HOST = "localhost";
+	private static final int PORT = 9875;
+
+	private static Socket socket;
 
 	public static void main(String[] args) {
-		 
-		String msg;
-		Ventana v = new Ventana("DNI Letter Client"); //Cliente y servidor con ventanas independientes
-		v.addText("Se ha iniciado \"DNI Letter Client\"");
-		v.addText("> Esperando NÃºmeros");
-		
-		Socket connection; // Socket para la comunicaciï¿½n con el servidor. Hay un socket (enchufe) en cada uno de los extremos
-		String host = "localhost"; // Identificador del host donde se ejecuta el servidor:  IP (p.e., 158.49.98.31)
-		int port=9875; // Puerto por el que escucha el servidor
-		//Solicitamos el DNI
-		BufferedReader incoming;	
+		System.out.println("Se ha iniciado \"DNI Letter Client\"");
+		System.out.println("> Esperando números");
+
+		// Solicitamos el DNI
+		int numero = -1;
 		Scanner sc = new Scanner(System.in);
-        int numero;
-        System.out.print("Por favor, ingrese su numero de DNI: ");
-        numero = sc.nextInt();
-        String DNInum = sc.nextLine();
-        System.out.println("Su nÃºmero es: " + DNInum);//Numeros puestos como string pero se puede quitar para dejarlo como int
-		
+		System.out.println("Por favor, ingrese su numero de DNI: ");
+		boolean nextInt = sc.hasNextInt();
+		while (!nextInt || numero < 0) {
+			System.out.println("La entrada no es un número válido");
+			System.out.println("Por favor, ingrese su número de DNI: ");
+			sc.nextLine();
+			nextInt = sc.hasNextInt();
+			if (nextInt)
+				numero = sc.nextInt();
+		}
+		System.out.println("Su número es: " + numero);
+		sc.close();
+
 		try {
+			// Conectamos con el server
+			System.out.println("Conectando a " + HOST + " por el puerto " + PORT);
+			socket = new Socket(HOST, PORT);
+			// Creamos el flujo I/O
 			PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())));
 			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			v.addText("Conectando a " + host + " por el puerto " + port);	
-			connection = new Socket(host, port); 
-			v.addText("          ........... conectado... a " + connection.getInetAddress().getHostName());
-		
+			System.out.println("Conectado a " + socket.getInetAddress().getHostName());
 
-		// EnvÃ­o del DNI
-			out.println(DNInum);
+			// Envío del DNI
+			out.println(numero);
 			out.flush();
-		// Lectura de la letra del servidor
-			msg=in.readLine();
-			while (msg !=null) {
-				v.addText(msg);
-				msg = in.readLine();
-			}
-			v.addText("end");
-			String letraDNI = in.readLine();
-			System.out.println("Su letra es:" + letraDNI);
-		// Cerramos el socket
-			connection.close();
+
+			// Lectura de la letra del servidor
+			String msg = in.readLine();
+			System.out.println("Su letra es: " + msg);
+			System.out.println("Terminando conexión...");
+
+			// Cerramos el socket
+			socket.close();
+			System.out.println("Fin de programa");
+
 		} catch (IOException e) {
 			System.err.println("Error de lectura del socket : " + e);
 			System.exit(-1);
 		}
 
-		
 	}
 
 }
